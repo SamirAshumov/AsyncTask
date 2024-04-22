1b7234b2373987d9a9f2eda8222af19f9b503f38
SamirAshumov <tu6KYLC6Z@code.edu.az>
2024-04-23 01:00:48 +0400

    uploaded

---
+++ BookStoreAsync/Program.cs
@@ -0,0 +1,43 @@
+﻿using Core.Models;
+namespace BookStoreAsync
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            BookStore bookStore = new BookStore();
+            bool exit = false;
+
+            while (!exit)
+            {                
+                Console.WriteLine("\nKitab magazasi");
+                Console.WriteLine("Menu:");
+                Console.WriteLine("1. Butun Kitablari gor");
+                Console.WriteLine("2. Kitab axtar");
+                Console.WriteLine("3. Kitab al");
+                Console.WriteLine("4. Cixis");
+                Console.Write("Secim edin : ");
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        await bookStore.DisplayBooksAsync();
+                        break;
+                    case "2":
+                        await bookStore.SearchBookAsync();
+                        break;
+                    case "3":
+                        await bookStore.PurchaseBookAsync();
+                        break;
+                    case "4":
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Secim yanlisdir! Yeniden daxil edin...");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Core/Models/BookStore.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    public class BookStore
+    {
+        private List<Book> books = new List<Book>()
+        {
+            new Book { Title = "Kitab 1", Author = "Author 1", Price = 18.75 },
+            new Book { Title = "Kitab 2", Author = "Author 2", Price = 22.50 },
+            new Book { Title = "Kitab 3", Author = "Author 3", Price = 12 },
+            new Book { Title = "Kitab 4", Author = "Author 4", Price = 18 },
+            new Book { Title = "Kitab 5", Author = "Author 5", Price = 15 },
+            new Book { Title = "Kitab 6", Author = "Author 6", Price = 19.40 }
+        };
+
+        public async Task DisplayBooksAsync()
+        {
+            Console.WriteLine("\nMovcud kitablarin siyahisi:");
+            foreach (var book in books)
+            {
+                await Task.Delay(500); 
+                Console.WriteLine($"Title: {book.Title}, Author: {book.Author}, Price: {book.Price} azn");
+            }
+        }
+
+        public async Task SearchBookAsync()
+        {
+            Console.Write("\nKitabin Title-ni daxil edin: ");
+            string title = Console.ReadLine();
+
+            var book = books.Find(b => b.Title.ToLower() == title.ToLower());
+            if (book != null)
+            {
+                await Task.Delay(500);
+                Console.WriteLine($"\nKitab tapildi - Title: {book.Title}, Author: {book.Author}, Price: {book.Price} azn");
+            }
+            else
+            {
+                await Task.Delay(300);
+                Console.WriteLine("\nKitab tapilmadi!");
+            }
+        }
+
+        public async Task PurchaseBookAsync()
+        {
+            Console.Write("\nAlmaq istediyiniz kitabin title-ni daxil edin: ");
+            string title = Console.ReadLine();
+
+            var book = books.Find(b => b.Title.ToLower() == title.ToLower());
+            if (book != null)
+            {
+                await Task.Delay(500);
+                Console.WriteLine($"\nKitabi aldiniz - Title: {book.Title}, Author: {book.Author}, Price: {book.Price} azn");
+            }
+            else
+            {
+                await Task.Delay(300); 
+                Console.WriteLine("\nKitab tapilmadi!");
+            }
+        }
+
+    }
+}
